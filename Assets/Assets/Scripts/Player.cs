243f3e26fb1f2aa58701ee73e61474a4578e0f46
Amark19 <amarkhamkar6@gmail.com>
2022-02-26 12:16:46 +0530

    first commit

---
+++ Assets/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool canMove;
+    public GameObject[] players;
+
+    // Update is called once per frame
+    void Update()
+    {
+    //     if (!canMove) return;
+        
+    //     for(int i = 0; i < Input.touchCount; i++)
+    //     {
+    //         foreach(var player in players){
+
+    //         Vector2 mousPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
+    //         Debug.Log(mousPos);
+    //         if (!player.GetComponent<CircleCollider2D>().OverlapPoint(mousPos))
+    //             return;
+    //         player.GetComponent<Rigidbody2D>().MovePosition(mousPos);
+    //         }
+        
+    //     }
+    }
+        
+    }
+
---
+++ Assets/Assets/Scripts/playerMov.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMov : MonoBehaviour
+{
+    //  public List<playerMov> Players = new List<playerMov>();
+     Rigidbody2D rb;
+
+    public Collider2D PlayerCollider { get; private set; }
+
+    public PlayerController Controller;
+
+    public int? LockedFingerID { get; set; }
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        PlayerCollider = GetComponent<Collider2D>();
+
+    }
+
+    public void MoveToPosition(Vector2 position)
+    {
+        
+        GetComponent<Rigidbody2D>().MovePosition(position);
+    }
+    private void OnEnable()
+    {
+        Controller.Players.Add(this);
+    }
+    // private void OnDisable()
+    // {
+    //     Controller.Players.Remove(this);
+    // }
+
+}
